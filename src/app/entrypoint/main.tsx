da5d113e4fe911ecf6b8b031f2a3191b184eb8ac
moonwqwizlio <tatsveet@gmail.com>
2026-03-29 19:00:26 +0300

    feat: vercel analytic added

--- src/app/entrypoint/main.tsx
+++ src/app/entrypoint/main.tsx
@@ -1,13 +1,14 @@
+import { Analytics } from '@vercel/analytics/next'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router/dom'
 import { router } from '../routes'
 import '../styles/index.css'
-
 const root = document.getElementById('root')
 
 createRoot(root!).render(
   <StrictMode>
+    <Analytics />
     <RouterProvider router={router} />
   </StrictMode>,
 )
